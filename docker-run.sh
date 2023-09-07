b411a9ca75a807c81aed1aa51c5dfc4588cc198f
birdgg <birdeggegg@gmail.com>
2023-09-07 19:19:29 +0800

    fix: typo

--- docker-run.sh
+++ docker-run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-command="node index.js --redis_host=${REDIS_HOST} --redis_port=${REDIS_PORT} --redis_password=${REDIS_PASSWORD} \ 
+command="node index.js --redis_host=${REDIS_HOST} --redis_port=${REDIS_PORT} --redis_password=${REDIS_PASSWORD} \
   --db_host=${MONGODB_HOST} --db_port=${MONGODB_PORT} --db_user=${MONGODB_USERNAME} --db_password=${MONGODB_PASSWORD} \
   --allowed_origins=${ALLOWED_ORIGINS} \
   --jwt_secret=${JWT_SECRET} \
