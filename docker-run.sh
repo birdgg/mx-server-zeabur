f4fad6025b5f517b93411ebf149e90833bb84d72
birdgg <birdeggegg@gmail.com>
2023-09-07 19:12:24 +0800

    fix: update scirpt

--- docker-run.sh
+++ docker-run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-command="node index.js --redis_host=127.0.0.1 --db_host=127.0.0.1 \
+command="node index.js --redis_host=${REDIS_HOST} --redis_port=${REDIS_PORT} --redis_password=${REDIS_PASSWORD} \ 
+  --db_host=${MONGODB_HOST} --db_port=${MONGODB_PORT} --db_user=${MONGODB_USERNAME} --db_password=${MONGODB_PASSWORD} \
   --allowed_origins=${ALLOWED_ORIGINS} \
   --jwt_secret=${JWT_SECRET} \
   --color"
