6f0c529ac818d75f92d88961a95122b15ce943e6
birdgg <birdeggegg@gmail.com>
2023-09-08 13:21:00 +0800

    fix: update .env

--- docker-run.sh
+++ docker-run.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
 command="node index.js --redis_host=${REDIS_HOST} --redis_port=${REDIS_PORT} --redis_password=${REDIS_PASSWORD} \
-  --db_host=${MONGODB_HOST} --db_port=${MONGODB_PORT} --db_user=${DB_USER} --db_password=${MONGODB_PASSWORD} \
+  --db_host=${MONGODB_HOST} --db_port=${MONGODB_PORT} --db_user=${MONGODB_USERNAME} --db_password=${MONGODB_PASSWORD} \
   --allowed_origins=${ALLOWED_ORIGINS} \
   --jwt_secret=${JWT_SECRET} \
   --color"
+
+if [ -n "$ENCRYPT_KEY" ]; then
+  command+=" --encrypt_key=${ENCRYPT_KEY}"
+fi
+
+if [ "$ENCRYPT_ENABLE" = "true" ]; then
+  command+=" --encrypt_enable "
+fi
 
 exec $command
